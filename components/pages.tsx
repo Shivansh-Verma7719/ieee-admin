1c7c19fe2002c8b0e78af9a380af901b30437e8a
Shivansh Verma <shivanshverma082004@gmail.com>
2024-10-12 21:21:40 +0530

    first commit

---
+++ components/pages.tsx
@@ -0,0 +1,9 @@
+export const getPages = () => {
+  return [
+    { name: "Home", href: "/" },  
+    { name: "Login", href: "/login" },
+    { name: "Logout", href: "/logout" },
+    { name: "New Event", href: "/new-event" },
+    { name: "Events", href: "/events" },
+  ];
+};
